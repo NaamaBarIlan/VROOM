7cba0d0ae2cbd7a5b4735cdc44a5398626535581
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-08-03 14:59:46 -0700

    Added seeded data migration. Added IEmployee interface Added EmployeeRepository Added EmployeeController All Employee CRUD routes are working.

--- VROOM/VROOM/Data/VROOMDbContext.cs
+++ VROOM/VROOM/Data/VROOMDbContext.cs
@@ -18,10 +18,51 @@ namespace VROOM.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            builder.Entity<Employee>().HasData(
-                new Employee { Id = 1, FirstName = "Michael" }
-                );
+            
             builder.Entity<EmployeeEquipmentItem>().HasKey(x => new { x.EmployeeId, x.EquipmentId });
+
+            // Seeding Employee Data:
+            builder.Entity<Employee>().HasData(
+                new Employee 
+                { 
+                    Id = 1,
+                    FirstName = "Michael",
+                    LastName = "Scott",
+                    Email = "mscott@vroom.com",
+                    Phone = "(570)-348-4178",
+                    Dept = "Management",
+                    Title = "Regional Manager",
+                    BranchName = "Scranton Branch",
+                    BranchAddress = "1725 Slough Avenue, Scranton, PA",
+                    BranchPhone = "(570) 348-4100"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Pamela",
+                    LastName = "Beesly",
+                    Email = "pbeesly@vroom.com",
+                    Phone = "(570) 348-4118",
+                    Dept = "Administration",
+                    Title = "Office Manager",
+                    BranchName = "Scranton Branch",
+                    BranchAddress = "1725 Slough Avenue, Scranton, PA",
+                    BranchPhone = "(570) 348-4100"
+                },
+                new Employee
+                {
+                    Id = 3,
+                    FirstName = "James",
+                    LastName = "Halpert",
+                    Email = "jhalpert@vroom.com",
+                    Phone = "(570) 348-4186",
+                    Dept = "Sales",
+                    Title = "Sales Representative",
+                    BranchName = "Scranton Branch",
+                    BranchAddress = "1725 Slough Avenue, Scranton, PA",
+                    BranchPhone = "(570) 348-4100"
+                }
+                );
         }
 
         // db sets
