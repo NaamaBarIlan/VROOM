fb903d3c0c76c6b80ba9e090f23549713702708d
Michael Refvem <mkrefvem@gmail.com>
2020-08-05 16:05:21 -0700

    Added front end to the API

---
+++ VROOM/VROOM/ViewModels/AllData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VROOM.Models.DTOs;
+
+namespace VROOM.ViewModels
+{
+    public class AllData
+    {
+        public List<EmployeeDTO> EmployeeList { get; set; }
+        public List<EquipmentItemDTO> EquipmentList { get; set; }
+    }
+}
